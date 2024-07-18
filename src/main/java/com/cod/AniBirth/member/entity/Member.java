75f23d39e080fee1af52bb5be69c891fb0f14f1e
psm817 <150203036+psm817@users.noreply.github.com>
2024-07-18 17:50:27 +0900

    헤더수정, member 세팅 (#4)
    * 헤더 추가
    
    * member setting

--- src/main/java/com/cod/AniBirth/member/entity/Member.java
+++ src/main/java/com/cod/AniBirth/member/entity/Member.java
@@ -1,8 +1,11 @@
 package com.cod.AniBirth.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -11,25 +14,18 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String username;
     private String password;
-    private String nickname;
+    private String nickname;    // 활동명 or 기업/보호소명
     private String email;
     private String phone;
-    private String authority;
     private String address;
-    private String company;
-    private Boolean isActive;
     private String thumbnailImg;
 
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+    private int authority;   // 보호소/기업(1) or 회원(2)
+    private int isActive;   // 승인(1), 대기(0)
 }
--- src/main/java/com/cod/AniBirth/member/service/MemberService.java
+++ src/main/java/com/cod/AniBirth/member/service/MemberService.java
@@ -1,18 +1,13 @@
 package com.cod.AniBirth.member.service;
 
 
-import com.cod.AniBirth.member.entity.Member;
 import com.cod.AniBirth.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
+@RequiredArgsConstructor
 public class MemberService {
-
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
 }
