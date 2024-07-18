8fd8fc9570ef7e7a6152bb2fbf14b9e5c8754fba
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-07-18 17:32:50 +0900

    add member

---
+++ src/main/java/com/cod/AniBirth/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.cod.AniBirth.member.repository;
+
+import com.cod.AniBirth.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+
+public interface MemberRepository extends JpaRepository <Member,Long>{
+
+}
---
+++ src/main/java/com/cod/AniBirth/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.cod.AniBirth.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+
+}
