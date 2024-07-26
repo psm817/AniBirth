713e98e33d248df8bf19e0e011ae02023f332c01
psm817 <5004jse@gmail.com>
2024-07-26 22:39:25 +0900

    봉사등록 시 카카오맵으로 장소 선택 구현

--- src/main/resources/static/javascript/volunteer/create.js
+++ src/main/resources/static/javascript/volunteer/create.js
@@ -64,3 +64,95 @@ function submitVolunteerForm(form) {
 
     form.submit();
 }
+
+// 이미지 미리보기
+function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function(){
+        var output = document.getElementById('preview');
+        output.src = reader.result;
+        output.style.display = 'block'; // 이미지가 로드되면 표시
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+// 카카오맵
+var map;
+var selectedPlace = null;
+
+function openKakaoMapModal() {
+    document.getElementById('kakaoMapModal').style.display = 'block';
+    initializeMap(); // 모달 열 때 지도 초기화
+}
+
+function closeKakaoMapModal() {
+    document.getElementById('kakaoMapModal').style.display = 'none';
+}
+
+function initializeMap() {
+    var mapContainer = document.getElementById('map'),
+        mapOption = {
+            center: new kakao.maps.LatLng(36.3527, 127.3857), // 대전 시청 좌표
+            level: 3
+        };
+
+    map = new kakao.maps.Map(mapContainer, mapOption);
+}
+
+function searchPlaces() {
+    var keyword = document.getElementById('keyword').value;
+    var places = new kakao.maps.services.Places();
+    places.keywordSearch(keyword, placesSearchCB);
+}
+
+function placesSearchCB(data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+        var listEl = document.getElementById('placesList'),
+            fragment = document.createDocumentFragment(),
+            bounds = new kakao.maps.LatLngBounds();
+
+        removeAllChildNodes(listEl);
+
+        for (var i = 0; i < data.length; i++) {
+            var itemEl = getListItem(i, data[i]);
+            fragment.appendChild(itemEl);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        listEl.appendChild(fragment);
+        map.setBounds(bounds);
+    }
+}
+
+function getListItem(index, places) {
+    var el = document.createElement('li');
+    el.innerHTML = places.place_name + '<br>' + places.address_name;
+    el.onclick = function() {
+        selectedPlace = places;
+        displayPlaceOnMap(places);
+        document.getElementById('selectButton').style.display = 'block';
+    };
+    return el;
+}
+
+function displayPlaceOnMap(place) {
+    var coords = new kakao.maps.LatLng(place.y, place.x);
+    map.setCenter(coords);
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: coords
+    });
+}
+
+function selectPlace() {
+    if (selectedPlace) {
+        document.getElementById('location').value = selectedPlace.address_name;
+        closeKakaoMapModal();
+    }
+}
+
+function removeAllChildNodes(el) {
+    while (el.hasChildNodes()) {
+        el.removeChild(el.lastChild);
+    }
+}
