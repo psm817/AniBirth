c9cdf427dcd67b2d85b44c34f5aac8c90d000de5
psm817 <5004jse@gmail.com>
2024-07-29 21:21:42 +0900

    봉사활동 신청 시 알림처리, 중복신청x

--- src/main/java/com/cod/AniBirth/volunteer/repository/VolunteerApplicationRepository.java
+++ src/main/java/com/cod/AniBirth/volunteer/repository/VolunteerApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.cod.AniBirth.volunteer.repository;
 
 import com.cod.AniBirth.member.entity.Member;
+import com.cod.AniBirth.volunteer.entity.Volunteer;
 import com.cod.AniBirth.volunteer.entity.VolunteerApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.Optional;
 @Repository
 public interface VolunteerApplicationRepository extends JpaRepository<VolunteerApplication, Long> {
     List<VolunteerApplication> findByVolunteerId(Long id);
+
+    boolean existsByMemberAndVolunteer(Member member, Volunteer volunteer);
 }
