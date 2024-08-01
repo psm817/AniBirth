d05867123c8081b03bb12360122eeda2dbd5600f
2251341 <dydwksos246@naver.com>
2024-08-01 14:11:25 +0900

    마켓

--- src/main/java/com/cod/AniBirth/global/initData/Dev.java
+++ src/main/java/com/cod/AniBirth/global/initData/Dev.java
@@ -117,17 +117,6 @@ public class Dev {
             volunteerReviewService.create("제목6", "내용6", 0, member10, "/images/volunteer/volunteer_default.jpg", null);
             volunteerReviewService.create("제목6", "내용6", 0, member10, "/images/volunteer/volunteer_default.jpg", null);
 
-            calendarService.create(volunteer1.getTitle(), start1, end1);
-            calendarService.create(volunteer2.getTitle(), start2, end2);
-            calendarService.create(volunteer3.getTitle(), start3, end3);
-            calendarService.create(volunteer4.getTitle(), start4, end4);
-            calendarService.create(volunteer5.getTitle(), start5, end5);
-            calendarService.create(volunteer6.getTitle(), start6, end6);
-            calendarService.create(volunteer7.getTitle(), start7, end7);
-            calendarService.create(volunteer8.getTitle(), start8, end8);
-            calendarService.create(volunteer9.getTitle(), start9, end9);
-            calendarService.create(volunteer10.getTitle(), start10, end10);
-            calendarService.create(volunteer11.getTitle(), start11, end11);
 
             productService.create("타이틀1","1 설명입니다",10000);
             productService.create("타이틀2","2 설명입니다",20000);
