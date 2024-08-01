58a0084dc23f3b6249a49c297be7bf5b5b00ab68
taewoo922 <155604554+taewoo922@users.noreply.github.com>
2024-08-01 16:44:12 +0900

    입양신청 #3 (#48)
    Co-authored-by: taewoo922 <taewoo922@gmail.com>

--- src/main/java/com/cod/AniBirth/adopt/AdoptForm.java
+++ src/main/java/com/cod/AniBirth/adopt/AdoptForm.java
@@ -1,9 +1,6 @@
 package com.cod.AniBirth.adopt;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,13 +8,30 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 public class AdoptForm {
-    @NotBlank(message="제목은 필수항목입니다.")
-    @Size(max=200, message = "제목을 200자 이하로 입력해주세요.")
+    @NotBlank(message="이름은 필수항목입니다.")
+    @Size(max=5, message = "이름을 5자 이하로 입력해주세요.")
     private String name;
 
-    @NotEmpty(message="내용은 필수항목입니다.")
-    @Size(max=20000, message = "내용을 20000자 이하로 입력해주세요.")
+    @NotBlank(message="연락처는 필수항목입니다.")
+    @Size(max=15, message = "연락처를 15자 이하로 입력해주세요.")
     private String phone;
 
-    private boolean isSupport;
+    @NotBlank(message="이메일은 필수항목입니다.")
+    @Email
+    private String email;
+
+    @NotBlank(message="나이는 필수항목입니다.")
+    private String age;
+
+    @NotBlank(message="직업/직장명은 필수항목입니다.")
+    private String company; //직업,직장명
+
+    private String postCode; //우편번호
+    private String address; //주소
+    private String detailAddress; //상세주소
+    private String extraAddress; //참고사항
+
+    private boolean isGender; //성별
+    private boolean isMarried; //결혼여부
+
 }
--- src/main/java/com/cod/AniBirth/adopt/service/AdoptService.java
+++ src/main/java/com/cod/AniBirth/adopt/service/AdoptService.java
@@ -2,6 +2,8 @@ package com.cod.AniBirth.adopt.service;
 
 import com.cod.AniBirth.adopt.AdoptForm;
 import com.cod.AniBirth.adopt.entity.Adopt;
+import com.cod.AniBirth.adopt.entity.AdoptApply;
+import com.cod.AniBirth.adopt.repository.AdoptApplyRepository;
 import com.cod.AniBirth.adopt.repository.AdoptRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,39 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AdoptService {
     private final AdoptRepository adoptRepository;
-    public void apply(String name, String phone) {
-        Adopt a = Adopt.builder()
+    private final AdoptApplyRepository adoptApplyRepository;
+
+    public void apply(String name, String phone, String email, String age, String company, String sample6Postcode, String sample6Address, String sample6DetailAddress, String sample6ExtraAddress, boolean isGender, boolean isMarried) {
+        // boolean 값을 String 값으로 변환
+        String gender = isGender ? "남자" : "여자";
+        String marriedStatus = isMarried ? "기혼" : "미혼";
+
+        AdoptApply adoptApply = AdoptApply.builder()
                 .name(name)
                 .phone(phone)
+                .age(age)
+                .email(email)
+                .company(company)
+                .postCode(sample6Postcode)
+                .address(sample6Address)
+                .detailAddress(sample6DetailAddress)
+                .extraAddress(sample6ExtraAddress)
+                .gender(gender)
+                .marriedStatus(marriedStatus)
                 .build();
-        adoptRepository.save(a);
+
+        adoptApplyRepository.save(adoptApply);
+
     }
+
+
+//    public void apply(String name, String phone) {
+//        Adopt a = Adopt.builder()
+//                .name(name)
+//                .phone(phone)
+//                .build();
+//        adoptRepository.save(a);
+//    }
 
 
 }
