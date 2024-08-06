576023b33686f70eecd6bd5873e36359706693ec
psm817 <5004jse@gmail.com>
2024-08-06 10:44:00 +0900

    페이지네이션 수정

--- src/main/java/com/cod/AniBirth/global/initData/Dev.java
+++ src/main/java/com/cod/AniBirth/global/initData/Dev.java
@@ -38,15 +38,15 @@ public class Dev {
         return args -> {
             // 회원 샘플
             Member member1 = memberService.signup("admin", "admin", "admin", "admin@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 0, 1);
-            Member member2 = memberService.signup("company1", "company1", "company1", "company1@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 1, 1);
-            Member member3 = memberService.signup("company2", "company2", "company2", "company2@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 1, 0);
-            Member member4 = memberService.signup("user1", "user1", "user1", "user1@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 2, 1);
-            Member member5 = memberService.signup("user2", "user2", "user2", "user2@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 2, 1);
-            Member member6 = memberService.signup("company3", "company3", "대전동물보호센터", "company3@test.com","010-1111-2222", "대전광역시 유성구 금남구즉로 1234 (금고동) 대전광역시 동물보호센터","/images/profile_default.jpg", 1, 1);
-            Member member7 = memberService.signup("company4", "company4", "세종유기동물보호센터", "company4@test.com","010-1111-2222", "세종특별자치시 전동면 미륵당1길 188 (전동면)","/images/profile_default.jpg", 1, 1);
-            Member member8 = memberService.signup("company5", "company5", "청양보호소", "company5@test.com","010-1111-2222", "충청남도 청양군 대치면 청산로 446-17 (대치면)","/images/profile_default.jpg", 1, 1);
-            Member member9 = memberService.signup("company6", "company6", "원주시동물보호센터", "company6@test.com","010-1111-2222", "강원특별자치도 원주시 호저면 칠봉로 109-17","/images/profile_default.jpg", 1, 1);
-            Member member10 = memberService.signup("user3", "user3", "user3", "user3@test.com","010-1111-2222", "대전광역시 서구 청사로 281","/images/profile_default.jpg", 2, 1);
+            Member member2 = memberService.signup("company1", "company1", "company1", "company1@test.com","042-123-4567", "대전광역시 유성구 금남구즉로 1234 (금고동) 대전광역시 동물보호센터","/images/profile_default.jpg", 1, 1);
+            Member member3 = memberService.signup("company2", "company2", "company2", "company2@test.com","042-123-4567", "대전광역시 유성구 금남구즉로 1234 (금고동) 대전광역시 동물보호센터","/images/profile_default.jpg", 1, 0);
+            Member member4 = memberService.signup("user1", "user1", "user1", "user1@test.com","010-1111-2222", "대전광역시 서구 대덕대로 179","/images/profile_default.jpg", 2, 1);
+            Member member5 = memberService.signup("user2", "user2", "user2", "user2@test.com","010-1111-2222", "대전광역시 서구 대덕대로 179","/images/profile_default.jpg", 2, 1);
+            Member member6 = memberService.signup("company3", "company3", "대전동물보호센터", "company3@test.com","042-123-4567", "대전광역시 유성구 금남구즉로 1234 (금고동) 대전광역시 동물보호센터","/images/profile_default.jpg", 1, 1);
+            Member member7 = memberService.signup("company4", "company4", "세종유기동물보호센터", "company4@test.com","042-123-4567", "세종특별자치시 전동면 미륵당1길 188 (전동면)","/images/profile_default.jpg", 1, 1);
+            Member member8 = memberService.signup("company5", "company5", "청양보호소", "company5@test.com","042-123-4567", "충청남도 청양군 대치면 청산로 446-17 (대치면)","/images/profile_default.jpg", 1, 1);
+            Member member9 = memberService.signup("company6", "company6", "원주시동물보호센터", "company6@test.com","042-123-4567", "강원특별자치도 원주시 호저면 칠봉로 109-17","/images/profile_default.jpg", 1, 1);
+            Member member10 = memberService.signup("user3", "user3", "user3", "user3@test.com","010-1111-2222", "대전광역시 서구 대덕대로 179","/images/profile_default.jpg", 2, 1);
 
             // 포인트 샘플
             accountService.createOrUpdate(member1, "123-1234-1234", 0L);
