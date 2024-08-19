6515a03fa9110cffedd83e60469936297c2a3e82
unknown <kimtaewoo922@gmail.com>
2024-08-19 17:38:36 +0900

    toast ui editor 완성

--- src/main/java/com/cod/AniBirth/adopt/entity/AdoptReview.java
+++ src/main/java/com/cod/AniBirth/adopt/entity/AdoptReview.java
@@ -3,6 +3,7 @@ package com.cod.AniBirth.adopt.entity;
 import com.cod.AniBirth.base.entity.BaseEntity;
 import com.cod.AniBirth.member.entity.Member;
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 public class AdoptReview extends BaseEntity {
     private String title;
+    @Column(nullable = false, length = 3000)
     private String content;
     private String images;
     private int hit;
