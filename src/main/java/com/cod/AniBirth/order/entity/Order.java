bc099ba58fa2beef18e02d012cdff0f3d2f9b23c
2251341 <161570121+2251341@users.noreply.github.com>
2024-08-23 16:05:42 +0900

    리뷰 완료, 구매상ㅇ품 리뷰 가능 (#115)

--- src/main/java/com/cod/AniBirth/order/entity/Order.java
+++ src/main/java/com/cod/AniBirth/order/entity/Order.java
@@ -1,39 +1,45 @@
-    package com.cod.AniBirth.order.entity;
+package com.cod.AniBirth.order.entity;
 
 
-    import com.cod.AniBirth.base.entity.BaseEntity;
-    import com.cod.AniBirth.member.entity.Member;
-    import jakarta.persistence.*;
-    import lombok.*;
-    import lombok.experimental.SuperBuilder;
+import com.cod.AniBirth.base.entity.BaseEntity;
+import com.cod.AniBirth.member.entity.Member;
+import com.cod.AniBirth.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-    import java.time.LocalDateTime;
-    import java.util.ArrayList;
-    import java.util.List;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-    import static jakarta.persistence.FetchType.LAZY;
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="product_order")
+public class Order extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member buyer;
 
-    @Entity
-    @SuperBuilder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Getter
-    @Setter
-    @Table(name="product_order")
-    public class Order extends BaseEntity {
-        @ManyToOne(fetch = LAZY)
-        private Member buyer;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
-        private String orderId;
-        private String name;
-        private boolean isPaid;
-        private boolean isCanceled;
-        private boolean isRefunded;
+    private String orderId;
+    private String name;
+    private boolean isPaid;
+    private boolean isCanceled;
+    private boolean isRefunded;
 
-        private Long totalPrice;
-        private LocalDateTime payDate;
+    private Long totalPrice;
+    private LocalDateTime payDate;
 
-        @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE)
-        @Builder.Default
-        private List<OrderItem> orderItemList = new ArrayList<>();
-    }
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE)
+    @Builder.Default
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+
+}
--- src/main/java/com/cod/AniBirth/order/repository/OrderRepository.java
+++ src/main/java/com/cod/AniBirth/order/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package com.cod.AniBirth.order.repository;
 
 import com.cod.AniBirth.member.entity.Member;
 import com.cod.AniBirth.order.entity.Order;
-import com.cod.AniBirth.order.entity.OrderItem;
 import com.cod.AniBirth.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,6 +14,8 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT oi.product FROM OrderItem oi JOIN oi.order o WHERE o.buyer = :member AND oi.isPaid = true")
     List<Product> findPurchasedProductsByMember(@Param("member") Member member);
+
+    boolean existsByBuyerAndProduct(Member buyer, Product product);
 
 
 }
